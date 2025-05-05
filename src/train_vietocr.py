679ffe5c64496ae33e08c59be961588f9b347c15
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2025-05-05 17:44:58 +0700

    feat: update using vit instead cnn

--- src/train_vietocr.py
+++ src/train_vietocr.py
@@ -9,9 +9,20 @@ from vietocr.tool.predictor import Predictor
 from vietocr.model.trainer import Trainer
 from vietocr.model.vocab import Vocab
 from vietocr.model.seqmodel import SeqModel
-from vietocr.model.cnn import CNN
 from vietocr.model.transformer import LanguageTransformer
-from vietocr.dataset import OCRDataset
+from transformers import ViTModel, ViTConfig
+import torch.nn as nn
+
+class ViTEncoder(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.vit = ViTModel.from_pretrained('google/vit-base-patch16-224')
+        self.projection = nn.Linear(768, config['transformer']['d_model'])
+        
+    def forward(self, x):
+        outputs = self.vit(x)
+        hidden_states = outputs.last_hidden_state
+        return self.projection(hidden_states)
 
 class CustomDataset(Dataset):
     def __init__(self, data_dir, labels_file, vocab, transform=None):
@@ -64,20 +75,36 @@ def create_vocab(labels_file):
     vocab = Vocab(chars)
     return vocab
 
+def create_vit_config():
+    config = {
+        'transformer': {
+            'd_model': 512,
+            'nhead': 8,
+            'num_encoder_layers': 6,
+            'num_decoder_layers': 6,
+            'dim_feedforward': 2048,
+            'dropout': 0.1,
+            'activation': 'relu',
+            'normalize_before': False,
+            'return_intermediate_dec': False
+        },
+        'train': {
+            'batch_size': 8,
+            'epochs': 100,
+            'print_every': 200,
+            'valid_every': 1000,
+            'checkpoint': './checkpoint/vit_transformer_checkpoint.pth',
+            'export': './weights/vit_transformer.pth',
+            'max_iters': 10000,
+            'dropout': 0.1,
+            'learning_rate': 1e-4
+        }
+    }
+    return config
+
 def train():
     # Configuration
-    config = Cfg.load_config_from_name('vgg_transformer')
-    
-    # Update config
-    config['train']['batch_size'] = 8
-    config['train']['epochs'] = 100
-    config['train']['print_every'] = 200
-    config['train']['valid_every'] = 1000
-    config['train']['checkpoint'] = './checkpoint/transformerocr_checkpoint.pth'
-    config['train']['export'] = './weights/vgg_transformer.pth'
-    config['train']['max_iters'] = 10000
-    config['train']['dropout'] = 0.1
-    config['train']['learning_rate'] = 1e-4
+    config = create_vit_config()
     
     # Create vocabulary
     vocab = create_vocab('dataset/labels.json')
@@ -98,9 +125,9 @@ def train():
     )
     
     # Create model
-    cnn = CNN(config['cnn'])
+    encoder = ViTEncoder(config)
     transformer = LanguageTransformer(config['transformer'])
-    model = SeqModel(cnn, transformer, vocab)
+    model = SeqModel(encoder, transformer, vocab)
     
     # Create trainer
     trainer = Trainer(model, config)
